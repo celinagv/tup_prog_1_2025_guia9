6f5a7298c4647c9d2c9b388f927ee892c4afa24e
Celina <guadalupesego421@gmail.com>
2025-06-01 22:11:29 -0300

    ejercicio1 a y b

---
+++ Guia_9/Ejecicio1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejecicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] serie;
+            int terminos;
+            int contador;
+            int valorAnterior = 0;
+            int valorActual = 0;
+            int proximoValor;
+
+
+            Console.WriteLine("Ingrese la cantidad de terminos deseada");
+            terminos = Convert.ToInt32(Console.ReadLine());
+            serie = new int[terminos];
+
+            contador = 1;
+
+            
+            valorActual = 1;
+            serie[1] = valorActual;
+            valorAnterior = 0;
+            serie[0] = valorAnterior;
+            
+
+            for (int i = 2; i < terminos; i++)
+            {
+                
+                    proximoValor = valorActual + valorAnterior;
+                    serie[i] = proximoValor;
+                    valorAnterior = valorActual;
+                    valorActual = proximoValor;
+               
+                contador++;
+            }
+
+
+            for (int i = 0;i < terminos;i++)
+            {
+                Console.WriteLine(serie[i]);
+            }
+        }
+    }
+}
---
+++ Guia_9/Ejercicio1_Recursivo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1_Recursivo
+{
+    internal class Program
+    {
+
+
+        static int terminos;
+     
+
+        static int CalculoFibonacci(int terminos)
+        {
+
+            if (terminos == 0 || terminos ==1)
+            {
+                return terminos;
+            }
+
+           return CalculoFibonacci(terminos -1) + CalculoFibonacci(terminos - 2);
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingrese la cantidad de terminos deseada");
+            terminos = Convert.ToInt32(Console.ReadLine());
+          
+            Console.WriteLine(CalculoFibonacci(terminos));
+
+
+
+
+        }
+    }
+}
